9958e28154ba890a53be4f89afd65fbfc38b803b
ramosvalentin606-ship-it <ramosvalentin606@gmail.com>
2026-04-09 07:48:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def encode_image(image_file):
 st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
 st.title("Análisis de Imagen:🤖🏞️")
+st.title("Pon lo que tu quieras")
 ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
 
