6e2f55fe70a8e975628091412b20bc70c8578654
ramosvalentin606-ship-it <ramosvalentin606@gmail.com>
2026-04-09 07:52:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 import base64
 from openai import OpenAI
+from PIL import Image
 
 # Function to encode the image to base64
 def encode_image(image_file):
@@ -12,6 +13,8 @@ st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_s
 # Streamlit page setup
 st.title("Análisis de Imagen:🤖🏞️")
 st.title("Pon lo que tu quieras")
+image = Image.open('Mbappe_Dictador.jpg')
+    st.image(image, width=350)
 ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
 
