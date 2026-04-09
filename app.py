5d5ffed3a9a93ae7c8583feab4f9274212146e78
ramosvalentin606-ship-it <ramosvalentin606@gmail.com>
2026-04-09 07:54:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_s
 # Streamlit page setup
 st.title("Análisis de Imagen:🤖🏞️")
 st.title("Pon lo que tu quieras")
-image = Image.open('Mbappe_Dictador.JPEG')
+image = Image.open('Mbappe_Dictador')
 st.image(image, width=350)
 ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
